60ffc92f7cb8ddf27e91c4f918fbb3a713e62f0a
Ryan Heaton <ryan@webcohesion.com>
2015-07-16 13:00:39 -0600

    using a more public developer key

--- src/main/java/org/familysearch/practice/App.java
+++ src/main/java/org/familysearch/practice/App.java
@@ -16,7 +16,7 @@ public class App {
 
   private String username = "lyonrw"; //Put username here
   private String password = "1234pass"; //Put password here
-  private String developerKey = "PWRD-4Y6P-8DNG-LTV9-XDD2-JBVG-R9BD-NRRP"; //Put developerKey here
+  private String developerKey = "WCQY-7J1Q-GKVV-7DNM-SQ5M-9Q5H-JX3H-CMJK"; //Put developerKey here
   private String pid = null;
   private FamilySearchFamilyTree ft = null;
 
